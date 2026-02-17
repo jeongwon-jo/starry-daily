48a5eca4fc2fa4a8c8a7581ff778428843665b91
jeongwon-jo <129711334+jeongwon-jo@users.noreply.github.com>
2026-02-17 16:34:19 +0900

    fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,49 +2,26 @@ import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+  console.log("🔥 CALLBACK HIT");
+
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
 
-  if (!code) {
-    console.log("❌ CODE 없음");
-    return NextResponse.redirect(
-      new URL("/login", request.url)
-    );
-  }
+  console.log("CODE:", code);
 
   const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.exchangeCodeForSession(code?? "");
+  const { data, error } =
+    await supabase.auth.exchangeCodeForSession(code ?? "");
 
   console.log("EXCHANGE ERROR:", error);
   console.log("SESSION:", data?.session);
 
-  if (error) {
-    console.log("❌", error)
-    return NextResponse.redirect(
-      new URL("/login", request.url)
-    );
-  }
-  
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user) {
-    console.log("❌ USER 없음");
-    return NextResponse.redirect(
-      new URL("/login", request.url)
-    );
-  }
-
-  const nickname = user?.user_metadata?.nickname;
-
-  if (!nickname) {
-    alert("nickname 없음")
-    return NextResponse.redirect(
-      new URL("/signup/step2", request.url)
-    );
-  }
+  console.log("USER:", user);
 
   return NextResponse.redirect(
     new URL("/", request.url)
