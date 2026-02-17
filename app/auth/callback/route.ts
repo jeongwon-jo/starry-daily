698150dbfd812f8bef1998a6d0300446e585cff0
jeongwon-jo <129711334+jeongwon-jo@users.noreply.github.com>
2026-02-17 16:36:20 +0900

    fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,26 +2,42 @@ import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  console.log("🔥 CALLBACK HIT");
-
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
 
-  console.log("CODE:", code);
+  if (!code) {
+    return NextResponse.redirect(
+      new URL("/login", request.url)
+    );
+  }
 
   const supabase = await createClient();
 
-  const { data, error } =
-    await supabase.auth.exchangeCodeForSession(code ?? "");
+  const { error } = await supabase.auth.exchangeCodeForSession(code);
 
-  console.log("EXCHANGE ERROR:", error);
-  console.log("SESSION:", data?.session);
-
+  if (error) {
+    return NextResponse.redirect(
+      new URL("/login", request.url)
+    );
+  }
+  
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  console.log("USER:", user);
+  if (!user) {
+    return NextResponse.redirect(
+      new URL("/login", request.url)
+    );
+  }
+
+  const nickname = user.user_metadata?.nickname;
+
+  if (!nickname) {
+    return NextResponse.redirect(
+      new URL("/signup/step2", request.url)
+    );
+  }
 
   return NextResponse.redirect(
     new URL("/", request.url)
